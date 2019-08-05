e2eae85433db88fb095fb0488743b30ccd93f7bb
Justin Merz <jrmerz@gmail.com>
2019-08-05 13:33:14 -0700

    quick fixes

--- app/elements/pages/about/app-page-about.tpl.js
+++ app/elements/pages/about/app-page-about.tpl.js
@@ -19,17 +19,17 @@ return html`
 <div style="max-width: 700px">
   <h1>PostGres Data Management System (PGDM) User Interface</h1>
 
-  The PGDM UI is a data management tool allowing user to administer their database using
+  The PGDM UI is a data management tool allowing users to administer their database using
   CSV spreadsheets.  Each sheet should represent a database view for a table.  The user 
   preforms INSERT / UPDATE / DELETE modifications by editing a spreadsheet and then uploading
   the sheet via PGDM. After each update a new version of the sheet can be downloaded and stored
   in cloud storage.
 
-  Sheets are versioned after each update preventing duplicate overwriting updates.  Sheets can be 
+  Sheets are versioned after each update preventing conflicting updates.  Sheets can be 
   completely removed from the system assuming no foreign key relations.
 
   <h2>Database Design & Code</h2>
-  The PGDM UI is a <a href="https://electronjs.org/">Electron</a> application built on top of the
+  The PGDM UI is an <a href="https://electronjs.org/">Electron</a> application built on top of the
   <a href="https://github.com/ucd-library/pgdm">PGDM Library</a>.  Please see the 
   <a href="https://github.com/ucd-library/pgdm/tree/master/docs">PGDM Library</a> for documentation on database design and
   implemention details.  The PGDM Library includes a CLI.
