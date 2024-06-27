87093cb2c1c5b71419e6cfe5a692781ec8b3b76a
Justin Merz <jrmerz@gmail.com>
2024-06-27 09:26:26 -0700

    fix latest electron version

--- main.js
+++ main.js
@@ -42,7 +42,8 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      contextIsolation: false
     }
   })
 
